53da522fb36932a11ff6041c695d4c72b8e6df6b
lnh <lnhcode@outlook.com>
2020-02-09 20:01:22 +0800

    Create github.ps1

---
+++ src/powershell/script/github.ps1
@@ -0,0 +1,17 @@
+git clone https://github.com/linianhui/aspnetcore.example
+git clone https://github.com/linianhui/blog
+git clone https://github.com/linianhui/cake.example
+git clone https://github.com/linianhui/channel9.downloader
+git clone https://github.com/linianhui/chrome.extensions
+git clone https://github.com/linianhui/cnblogs.theme
+git clone https://github.com/linianhui/code
+git clone https://github.com/linianhui/code.guide
+git clone https://github.com/linianhui/div
+git clone https://github.com/linianhui/docker
+git clone https://github.com/linianhui/git.web
+git clone https://github.com/linianhui/http.benchmark
+git clone https://github.com/linianhui/linianhui.github.io
+git clone https://github.com/linianhui/networking
+git clone https://github.com/linianhui/nuget.server
+git clone https://github.com/linianhui/oidc.example
+git clone https://github.com/linianhui/spring.example
