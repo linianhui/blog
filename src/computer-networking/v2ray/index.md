c36b59c51dbacc538b83e1d3d49928245cdf6ec4
lnh <lnhcode@outlook.com>
2020-02-13 09:11:44 +0800

    Add v2ray

---
+++ src/computer-networking/v2ray/index.md
@@ -0,0 +1,17 @@
+---
+title: "[计算机网络] v2ray"
+created_at: 2020-02-13 08:01:00
+toc: true
+---
+
+# 1 安装脚本 {#1-install-script}
+
+{{<highlight-file file="install.sh" lang="sh">}}
+
+# 2 服务端配置 {#2-server-config}
+
+{{<highlight-file file="server-config.template.json" lang="json">}}
+
+# 2 客户端配置 {#3-client-config}
+
+{{<highlight-file file="client-config.template.json" lang="json">}}
---
+++ src/computer-networking/v2ray/install.sh
@@ -0,0 +1,26 @@
+# wget https://linianhui.github.io/computer-networking/v2ray/install.sh
+# bash install.sh
+
+set -eux
+
+bash <(wget -O - https://install.direct/go.sh)
+
+apt install gettext-base
+
+export PATH=$PATH:/usr/bin/v2ray/
+
+SERVER_NAME=domain.test
+UUID=$(v2ctl uuid)
+CERTIFICATE_JSON=$(v2ctl cert --ca --domain=$SERVER_NAME --expire=24000h --name=$SERVER_NAME --org=$SERVER_NAME)
+
+wget https://linianhui.github.io/computer-networking/v2ray/client-config.template.json -O client-config.template.json
+envsubst < server-config.template.json > /etc/v2ray/config.json
+
+wget https://linianhui.github.io/computer-networking/v2ray/server-config.template.json -O server-config.template.json
+envsubst < client-config.template.json > client-config.json
+
+service v2ray stop
+service v2ray start
+service v2ray status
+
+lsof -i:443
