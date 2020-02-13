914024f8f1ad839721da639689dd4a44ab83b616
lnh <lnhcode@outlook.com>
2020-02-13 09:53:10 +0800

    Update install.sh

--- src/computer-networking/v2ray/install.sh
+++ src/computer-networking/v2ray/install.sh
@@ -8,10 +8,9 @@ bash <(wget -O - https://install.direct/go.sh)
 apt install gettext-base
 
 export PATH=$PATH:/usr/bin/v2ray/
-
-SERVER_NAME=domain.test
-UUID=$(v2ctl uuid)
-CERTIFICATE_JSON=$(v2ctl cert --ca --domain=$SERVER_NAME --expire=24000h --name=$SERVER_NAME --org=$SERVER_NAME)
+export SERVER_NAME=domain.test
+export UUID=$(v2ctl uuid)
+export CERTIFICATE_JSON=$(v2ctl cert --ca --domain=$SERVER_NAME --expire=24000h --name=$SERVER_NAME --org=$SERVER_NAME)
 
 wget https://linianhui.github.io/computer-networking/v2ray/server-config.template.json -O server-config.template.json
 envsubst < server-config.template.json > /etc/v2ray/config.json
