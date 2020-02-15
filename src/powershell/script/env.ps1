0160c2cfd66837768f0c66f263a2f36d93ba193b
lnh <lnhcode@outlook.com>
2020-02-15 12:57:07 +0800

    Add SSLKEYLOGFILE

--- src/powershell/script/env.ps1
+++ src/powershell/script/env.ps1
@@ -15,19 +15,19 @@ function script:Env-TrySetVariable (
 
     # 输出旧值
     [string]$OldValue = [System.Environment]::GetEnvironmentVariable($Variable, $Target);
-    Log-Info "[$Target][$Variable] 旧值:" $OldValue
+    Log-Info "[$Target][$Variable] OLD_VALUE : " $OldValue
 
     if ($OldValue -eq $Value) {
-        Log-Warn "[$Target][$Variable] 新值:" $Value
+        Log-Warn "[$Target][$Variable] NEW_VALUE : " $Value
         return
     }
 
     # 设置环境变量
     [System.Environment]::SetEnvironmentVariable($Variable, $Value, $Target)
-    
+
     # 输出新值
     [string]$NewValue = [System.Environment]::GetEnvironmentVariable($Variable, $Target);
-    Log-Debug "[$Target][$Variable] 新值:" $NewValue
+    Log-Debug "[$Target][$Variable] NEW_VALUE : " $NewValue
     Write-Host
 }
 
@@ -41,11 +41,11 @@ function script:Env-TryAppendPathVariable (
 
     # 检测是否已经存在
     if ($OldValue.Split(';').Contains($Value)) {
-        Log-Warn "[$Target][$Variable] 已存在：$Value"
+        Log-Warn "[$Target][$Variable] EXISTS_VALUE : $Value"
         return
     }
 
-    Log-Debug "[$Target][$Variable] 追加：$Value"
+    Log-Debug "[$Target][$Variable] APPEND_VALUE : $Value"
 
     # 追加新值
     [string]$NewValue = $OldValue;
@@ -62,7 +62,7 @@ function script:Env-TryAppendPathVariable (
 
 function Env-TrySetAll() {
     Get-Command -Name 'Env-Set*EnvironmentVariable' -CommandType Function | ForEach-Object {
-        Log-Info "执行: $($_.Name)"
+        Log-Info "RUN : $($_.Name)"
         Invoke-Expression -Command $_.Name
         Write-Host
         Write-Host
@@ -174,7 +174,7 @@ function Env-SetKotlinEnvironmentVariable() {
 # https://dotnet.github.io/
 # https://docs.microsoft.com/en-us/dotnet/core/tools/telemetry
 function Env-SetNetEnvironmentVariable() {
-    Env-TrySetVariable -Variable 'DOTNET_CLI_TELEMETRY_OPTOUT' -Value "true" 
+    Env-TrySetVariable -Variable 'DOTNET_CLI_TELEMETRY_OPTOUT' -Value "true"
 }
 
 # https://www.python.org/downloads/windows/
@@ -194,7 +194,7 @@ function Env-SetILSpyEnvironmentVariable() {
     Env-TryAppendPathVariable -Value $APP_ILSPY_DIR
 }
 
-# https://www.nuget.org/downloads 
+# https://www.nuget.org/downloads
 # https://docs.microsoft.com/en-us/nuget/tools/cli-ref-environment-variables
 function Env-SetNugetEnvironmentVariable() {
     $APP_NUGET_DIR = $APP_DIR + '.nuget\';
@@ -424,7 +424,7 @@ function Env-SetRustEnvironmentVariable() {
     Env-TrySetVariable -Variable 'RUSTUP_HOME' -Value $APP_RUSTUP_DIR
     Env-TrySetVariable -Variable 'RUSTUP_DIST_SERVER' -Value 'http://mirrors.ustc.edu.cn/rust-static'
     Env-TrySetVariable -Variable 'RUSTUP_UPDATE_ROOT' -Value 'http://mirrors.ustc.edu.cn/rust-static/rustup'
-    
+
     # https://github.com/rust-lang/cargo/blob/master/src/doc/src/reference/environment-variables.md
     Env-TrySetVariable -Variable 'CARGO_HOME' -Value $APP_CARGO_DIR
 
@@ -439,4 +439,14 @@ function Env-SetHugoEnvironmentVariable() {
     $APP_HUGO_DIR = $APP_DIR + '.hugo\';
 
     Env-TryAppendPathVariable -Value $APP_HUGO_DIR
+}
+
+# https://developer.mozilla.org/en-US/docs/Mozilla/Projects/NSS/Key_Log_Format
+# https://wiki.wireshark.org/TLS
+function Env-SetSslKeyLogFileEnvironmentVariable() {
+    $CACHE_SSL_DIR = $CACHE_DIR + '.ssl\';
+
+    $SSLKEYLOGFILE = $CACHE_SSL_DIR + 'ssl.log';
+
+    Env-TrySetVariable -Variable 'SSLKEYLOGFILE' -Value $SSLKEYLOGFILE
 }
