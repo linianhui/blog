2ae8ac61401d9c99e96c497e6efa0d54f96760b1
lnh <lnhcode@outlook.com>
2020-06-14 11:09:28 +0800

    update script

--- src/computer-networking/v2ray/install.sh
+++ src/computer-networking/v2ray/install.sh
@@ -1,5 +1,5 @@
 # wget https://linianhui.github.io/computer-networking/v2ray/install.sh
-# bash install.sh '0.0.0.0' '0.0.0.0' 'domain1.test' $(cat /proc/sys/kernel/random/uuid)
+# bash install.sh '0.0.0.0'
 
 set -eux
 
@@ -7,15 +7,17 @@ apt install -y wget gettext-base
 
 bash <(wget -q -O - https://install.direct/go.sh)
 
-export TEMPLATE_SERVER_LISTEN_IP=$1
-export TEMPLATE_SERVER_IP=$2
-export TEMPLATE_SERVER_NAME=$3
-export TEMPLATE_CLIENT_ID=$4
-export TEMPLATE_CERTIFICATE_JSON=$(/usr/bin/v2ray/v2ctl cert -json --domain=$TEMPLATE_SERVER_NAME --expire=24000h --name=name.test --org=org.test)
+CRET_NAME=$(cat /proc/sys/kernel/random/uuid)
+CRET_ORG=$(cat /proc/sys/kernel/random/uuid)
+
+export TEMPLATE_SERVER_IP=$1
+export TEMPLATE_SERVER_DOMAIN_NAME="$(cat /proc/sys/kernel/random/uuid).test"
+export TEMPLATE_CLIENT_ID=$(cat /proc/sys/kernel/random/uuid)
+export TEMPLATE_CERTIFICATE_JSON=$(/usr/bin/v2ray/v2ctl cert -json --domain=$TEMPLATE_SERVER_DOMAIN_NAME --expire=24000h --name=$CRET_NAME --org=$CRET_ORG)
 
 wget -q -O - https://linianhui.github.io/computer-networking/v2ray/server-config.template.json | envsubst > /etc/v2ray/config.json
 
-wget -q -O - https://linianhui.github.io/computer-networking/v2ray/client-config.template.json | envsubst > client-config.json
+wget -q -O - https://linianhui.github.io/computer-networking/v2ray/client-config.template.json | envsubst > client/config.json
 
 service v2ray restart
 service v2ray status
