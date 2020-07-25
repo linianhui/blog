d45879798f9c9d61845b047b6fdf7448cfdd5977
lnh <lnhcode@outlook.com>
2020-07-25 11:14:33 +0800

    Update code.ps1

--- src/powershell/script/code.ps1
+++ src/powershell/script/code.ps1
@@ -1,33 +1,30 @@
 # 代码的根目录
 $CODE_PATH = 'd:\.code\';
-# 我的代码的根目录
-$MY_CODE_PATH = $CODE_PATH + '.lnh\';
 
 $CODE_PATH_ALIAS = @{
-    g   = $CODE_PATH;
     ps  = 'd:\.powershell\';
     w   = 'd:\.work\';
-    ae  = $MY_CODE_PATH + 'aspnetcore.example';
-    ce  = $MY_CODE_PATH + 'cake.example';
-    c9d = $MY_CODE_PATH + 'channel9.downloader';
-    ces = $MY_CODE_PATH + 'chrome.extensions';
-    ct  = $MY_CODE_PATH + 'cnblogs.theme';
-    c   = $MY_CODE_PATH + 'code';
-    cg  = $MY_CODE_PATH + 'code.guide';
-    d   = $MY_CODE_PATH + 'docker';
-    gw  = $MY_CODE_PATH + 'git.web';
-    hb  = $MY_CODE_PATH + 'http.benchmark';
-    di  = $MY_CODE_PATH + 'div';
-    b   = $MY_CODE_PATH + 'blog';
-    n   = $MY_CODE_PATH + 'networking';
-    ns  = $MY_CODE_PATH + 'nuget.server';
-    oe  = $MY_CODE_PATH + 'oidc.example';
-    se  = $MY_CODE_PATH + 'spring.example';
+    ae  = $CODE_PATH + 'aspnetcore.example';
+    ce  = $CODE_PATH + 'cake.example';
+    c9d = $CODE_PATH + 'channel9.downloader';
+    ces = $CODE_PATH + 'chrome.extensions';
+    ct  = $CODE_PATH + 'cnblogs.theme';
+    c   = $CODE_PATH + 'code';
+    cg  = $CODE_PATH + 'code.guide';
+    d   = $CODE_PATH + 'docker';
+    gw  = $CODE_PATH + 'git.web';
+    hb  = $CODE_PATH + 'http.benchmark';
+    di  = $CODE_PATH + 'div';
+    b   = $CODE_PATH + 'blog';
+    n   = $CODE_PATH + 'networking';
+    ns  = $CODE_PATH + 'nuget.server';
+    oe  = $CODE_PATH + 'oidc.example';
+    se  = $CODE_PATH + 'spring.example';
 }
 
 function Code-ChangeDirectory ([string] $Alias) {
-    $TargetPath = $MY_CODE_PATH
-    
+    $TargetPath = $CODE_PATH
+
     if ($Alias) {
         $TargetPath= $CODE_PATH_ALIAS[$Alias]
     }
@@ -37,8 +34,8 @@ function Code-ChangeDirectory ([string] $Alias) {
 }
 
 function Code-UseVsCodeOpenDirectory ([string] $Alias) {
-    $TargetPath = $MY_CODE_PATH
-    
+    $TargetPath = $CODE_PATH
+
     if ($Alias) {
         $TargetPath= $CODE_PATH_ALIAS[$Alias]
     }
