c8ce2d8c022ca50a34a40da6dfdff5342759e36d
lnh <lnhcode@outlook.com>
2020-08-12 17:17:50 +0800

    Update v2ray install.sh

--- src/tool/v2ray/install.sh
+++ src/tool/v2ray/install.sh
@@ -4,31 +4,31 @@
 
 set -eux
 
-apt install -y wget gettext-base
+yum install -y wget gettext
 
-bash <(wget -q -O - https://install.direct/go.sh)
+bash <(wget -q -O - https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
 
-CRET_NAME=$(/usr/bin/v2ray/v2ctl uuid)
-CRET_ORG=$(/usr/bin/v2ray/v2ctl uuid)
+CRET_NAME=$(v2ctl uuid)
+CRET_ORG=$(v2ctl uuid)
 
 export TEMPLATE_SERVER_IP=$1
-export TEMPLATE_SERVER_DOMAIN_NAME="$(/usr/bin/v2ray/v2ctl uuid).test"
-export TEMPLATE_CLIENT_ID=$(/usr/bin/v2ray/v2ctl uuid)
-export TEMPLATE_CERTIFICATE_JSON=$(/usr/bin/v2ray/v2ctl cert --ca --json --domain=$TEMPLATE_SERVER_DOMAIN_NAME --expire=24000h --name=$CRET_NAME --org=$CRET_ORG)
+export TEMPLATE_SERVER_DOMAIN_NAME="$(v2ctl uuid).test"
+export TEMPLATE_CLIENT_ID=$(v2ctl uuid)
+export TEMPLATE_CERTIFICATE_JSON=$(v2ctl cert --ca --json --domain=$TEMPLATE_SERVER_DOMAIN_NAME --expire=24000h --name=$CRET_NAME --org=$CRET_ORG)
 
-wget -q -O - https://linianhui.github.io/tool/v2ray/server-config.template.json | envsubst > /etc/v2ray/config.json
+wget -q -O - https://linianhui.github.io/tool/v2ray/server-config.template.json | envsubst > /usr/local/etc/v2ray/config.json
 
 wget -q -O - https://linianhui.github.io/tool/v2ray/client-config.template.json | envsubst > client-config.json
 
 echo "vmess://$(wget -q -O - https://linianhui.github.io/tool/v2ray/client-config.v2rayng.template.json | envsubst | base64 -w 0)" > client-config.v2rayng.json
 
-service v2ray restart
-service v2ray status
+systemctl enable v2ray
+systemctl restart v2ray
+systemctl status v2ray
 
 lsof -i:443
 
-# https://github.com/v2ray/v2ray-core
-# https://github.com/v2ray/manual/blob/master/zh_cn/SUMMARY.md
+# https://github.com/v2fly/v2ray-core
 
 # https://github.com/gfwlist/gfwlist
 # https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt
