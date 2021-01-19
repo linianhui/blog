4f8752bba8b89e023e0a055f0df05b8c5a6c81b6
lnh <lnhcode@outlook.com>
2021-01-19 22:18:14 +0800

    Fix v2fly

--- src/tool/v2ray/install.sh
+++ src/tool/v2ray/install.sh
@@ -4,7 +4,7 @@
 
 set -eux
 
-yum install -y wget gettext lsof
+apt install -y wget gettext lsof
 
 bash <(wget -q -O - https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
 
@@ -12,16 +12,13 @@ UUID=$(/usr/local/bin/v2ctl uuid)
 CRET_NAME=${UUID:0:8}
 CRET_ORG=${UUID:9:4}
 
-export TEMPLATE_SERVER_IP=$1
+export TEMPLATE_LISTEN_IP=$1
+export TEMPLATE_SERVER_IP=$2
 export TEMPLATE_SERVER_DOMAIN_NAME="${UUID:24:12}.test"
 export TEMPLATE_CLIENT_ID=$UUID
 export TEMPLATE_CERTIFICATE_JSON=$(/usr/local/bin/v2ctl cert --ca --json --domain=$TEMPLATE_SERVER_DOMAIN_NAME --expire=24000h --name=$CRET_NAME --org=$CRET_ORG)
 
-rm -rf /etc/systemd/system/v2ray.service.d
-
-wget -q https://linianhui.github.io/tool/v2ray/v2ray.service -O /etc/systemd/system/v2ray.service
-
-wget -q -O - https://linianhui.github.io/tool/v2ray/server-config.template.json | envsubst > /etc/v2ray.json
+wget -q -O - https://linianhui.github.io/tool/v2ray/server-config.template.json | envsubst > /usr/local/etc/v2ray/config.json
 
 wget -q -O - https://linianhui.github.io/tool/v2ray/client-config.template.json | envsubst > client-config.json
 
