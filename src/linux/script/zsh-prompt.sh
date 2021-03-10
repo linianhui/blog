cff071b0becbbd54d629a9e5de294473f2479a40
lnh <lnhdyx@outlook.com>
2021-03-10 12:10:47 +0800

    Update zsh-prompt.sh

--- src/linux/script/zsh-prompt.sh
+++ src/linux/script/zsh-prompt.sh
@@ -1,16 +1,24 @@
 # zsh              http://www.zsh.org/
 # precmd           http://zsh.sourceforge.net/Doc/Release/Functions.html#Hook-Functions
+# Prompt-Expansion http://zsh.sourceforge.net/Doc/Release/Prompt-Expansion.html#Prompt-Expansion
 # __posh_git_echo  https://github.com/lyze/posh-git-sh
+
+autoload -U colors && colors
+
+setopt prompt_subst
+
+RPROMPT="[%(?.%F{green}%?%f.%F{red}%?%f)]"
+
 precmd(){
   USER_TYPE='%(!.#.$)'
   NEW_LINE=$'\n'
-  COMMON_PROMPT='$NEW_LINE%{$fg[green]%}$USER_TYPE %n@%M%{$reset_color%} %d %T'
-  INPUT_PROMPT='$NEW_LINE%{$fg[green]%}$USER_TYPE %{$reset_color%}'
+  COMMON_PROMPT='$NEW_LINE%F{green}$USER_TYPE%f %n@%m %d %D %T'
+  INPUT_PROMPT='$NEW_LINE%F{green}$USER_TYPE %f'
   if git rev-parse --git-dir > /dev/null 2>&1; then
     GIT_USER="$(git config user.name)@<$(git config user.email)>"
-    GIT_PROMPT='$NEW_LINE%{$fg[green]%}$USER_TYPE $GIT_USER%{$reset_color%} %{$fg[red]%}%1d%{$reset_color%} $(__posh_git_echo)'
-    PS1="$COMMON_PROMPT $GIT_PROMPT $INPUT_PROMPT"
+    GIT_PROMPT='$NEW_LINE%F{green}$USER_TYPE $GIT_USER%f %F{red}%1d%f $(__posh_git_echo)'
+    PROMPT="$COMMON_PROMPT $GIT_PROMPT $INPUT_PROMPT"
   else
-    PS1="$COMMON_PROMPT $INPUT_PROMPT"
+    PROMPT="$COMMON_PROMPT $INPUT_PROMPT"
   fi
 }
