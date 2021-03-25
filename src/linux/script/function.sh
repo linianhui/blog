176daaeeb83275548748e04528492c8ebacf0a87
lnh <lnhdyx@outlook.com>
2021-03-25 17:49:21 +0800

    add git_proxy function

--- src/linux/script/function.sh
+++ src/linux/script/function.sh
@@ -1,9 +1,9 @@
 function backup_file() {
-  cp "$1" "$1.bak";
+    cp "$1" "$1.bak";
 }
 
 function port(){
-  lsof "-i:$1"
+    lsof "-i:$1"
 }
 
 function git_warn_gc () {
@@ -16,9 +16,26 @@ function git_warn_gc () {
 }
 
 function proc_env(){
-  cat /proc/$1/environ | tr '\0' '\n' | sort
+    cat /proc/$1/environ | tr '\0' '\n' | sort
 }
 
 function proc_env2(){
-  ps ewww -o command $1 | tr ' ' '\n' | sort
+    ps ewww -o command $1 | tr ' ' '\n' | sort
+}
+
+function git_proxy(){
+    local action=${1:-get}
+
+    if [ $action = 'set' ] ;then
+        git config --global http.proxy 'socks5://127.0.0.1:1080'
+        git config --global https.proxy 'socks5://127.0.0.1:1080'
+    fi
+
+    if [ $action = 'unset' ] ;then
+        git config --global --unset http.proxy
+        git config --global --unset https.proxy
+    fi
+
+    git config --global --get http.proxy
+    git config --global --get https.proxy
 }
