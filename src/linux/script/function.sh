500dc00b4e73f46e614fa11bacbcf199b6213d12
lnh <lnhdyx@outlook.com>
2021-04-23 13:41:03 +0800

    remove git https.proxy

--- src/linux/script/function.sh
+++ src/linux/script/function.sh
@@ -28,7 +28,6 @@ function git_proxy(){
 
     if [ $action = 'set' ] ;then
         git config --global http.proxy 'socks5://127.0.0.1:1080'
-        git config --global https.proxy 'socks5://127.0.0.1:1080'
     fi
 
     if [ $action = 'unset' ] ;then
--- src/powershell/script/git.ps1
+++ src/powershell/script/git.ps1
@@ -222,7 +222,6 @@ function Git-Proxy(
 ) {
     if ($set) {
         git config --global http.proxy 'socks5://127.0.0.1:1080'
-        git config --global https.proxy 'socks5://127.0.0.1:1080'
     }
 
     if ($unset) {
