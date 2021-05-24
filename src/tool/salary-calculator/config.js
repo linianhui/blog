211d76aaa3710162852c09e5554504d41d15f9b3
lnh <lnhdyx@outlook.com>
2021-05-24 18:18:59 +0800

    add hangzhou
    --ci

--- src/tool/salary-calculator/config.js
+++ src/tool/salary-calculator/config.js
@@ -86,6 +86,44 @@ var config = {
                 corporationPercentage: 12,
                 personalPercentage: 12
             }
+        },
+        "杭州2021社保基数": {
+            养老: {
+                min: 3321.6,
+                max: 17880.75,
+                corporationPercentage: 14,
+                personalPercentage: 8
+            },
+            医疗: {
+                min: 3321.6,
+                max: 17880.75,
+                corporationPercentage: 10.5,
+                personalPercentage: 2
+            },
+            失业: {
+                min: 3321.6,
+                max: 17880.75,
+                corporationPercentage: 0.5,
+                personalPercentage: 0.5
+            },
+            工伤: {
+                min: 3321.6,
+                max: 17880.75,
+                corporationPercentage: 0.4,
+                personalPercentage: 0
+            },
+            生育: {
+                min: 3321.6,
+                max: 17880.75,
+                corporationPercentage: 1.2,
+                personalPercentage: 0
+            },
+            公积金: {
+                min: 0,
+                max: 17880.75,
+                corporationPercentage: 12,
+                personalPercentage: 12
+            }
         }
     }
 };
