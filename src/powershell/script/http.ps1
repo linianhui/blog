e7e1dc6e3221016b85428f66d3dd38d8ffbafbf4
lnh <lnhdyx@outlook.com>
2021-06-27 10:17:10 +0800

    Update http.ps1
    --ci

--- src/powershell/script/http.ps1
+++ src/powershell/script/http.ps1
@@ -1,4 +1,5 @@
 
+# https://github.com/svenstaro/miniserve
 function Http-Server() {
     Log-Debug "miniserve --color-scheme squirrel --qrcode --upload-files[-u]" $Args
     miniserve --color-scheme squirrel --qrcode $Args
