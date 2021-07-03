ab081f56a2b4136f5aac00994e7689c98de7720b
lnh <lnhdyx@outlook.com>
2021-07-03 08:21:21 +0800

    change LAST_EXIT_CODE to PROMPT

--- src/linux/script/zsh-prompt.sh
+++ src/linux/script/zsh-prompt.sh
@@ -3,16 +3,15 @@
 # Prompt-Expansion http://zsh.sourceforge.net/Doc/Release/Prompt-Expansion.html#Prompt-Expansion
 # __posh_git_echo  https://github.com/lyze/posh-git-sh
 
-unsetopt PROMPT_SP
-
-setopt prompt_subst
-
-RPROMPT="[%(?.%F{green}%?%f.%F{red}%?%f)]"
+# unsetopt PROMPT_SP
+# setopt prompt_subst
+# RPROMPT="[%(?.%F{green}%?%f.%F{red}%?%f)]"
 
 precmd(){
   USER_TYPE='%(!.#.$)'
   NEW_LINE=$'\n'
-  COMMON_PROMPT='$NEW_LINE%F{green}$USER_TYPE%f %n@%m %d %D %*'
+  LAST_EXIT_CODE="%(?..%F{red}%?%f)"
+  COMMON_PROMPT='$NEW_LINE%F{green}$USER_TYPE%f %n@%m %d %D %* $LAST_EXIT_CODE'
   INPUT_PROMPT='$NEW_LINE%F{green}$USER_TYPE %f'
   if git rev-parse --git-dir > /dev/null 2>&1; then
     GIT_USER="$(git config user.name)@<$(git config user.email)>"
