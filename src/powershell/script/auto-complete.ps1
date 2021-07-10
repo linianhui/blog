faae226414e4fbc4207dfdb30318cf7bc8927e6b
lnh <lnhdyx@outlook.com>
2021-07-10 12:19:45 +0800

    Update auto-complete.ps1

--- src/powershell/script/auto-complete.ps1
+++ src/powershell/script/auto-complete.ps1
@@ -1,3 +1,5 @@
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/register-argumentcompleter?view=powershell-7.1
+
 Register-ArgumentCompleter -CommandName Directory-To -ParameterName Path -ScriptBlock $__QUICK_ACCESS_DIRECTORY_SCRIPT_BLOCK
 Register-ArgumentCompleter -CommandName VsCode-Open -ParameterName Path -ScriptBlock $__QUICK_ACCESS_DIRECTORY_SCRIPT_BLOCK
 Register-ArgumentCompleter -CommandName Idea-Open -ParameterName Path -ScriptBlock $__QUICK_ACCESS_DIRECTORY_SCRIPT_BLOCK
