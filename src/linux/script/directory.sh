67737c8c56117873dbf74fb5ff04ae1299a08c35
lnh <lnhdyx@outlook.com>
2021-07-11 16:19:56 +0800

    Update directory.sh

--- src/linux/script/directory.sh
+++ src/linux/script/directory.sh
@@ -1,29 +1,33 @@
 declare -a __QUICK_ACCESS_DIRECTORY=()
 
 function __directory_add_to_quick_access {
-  dir="$1"
-  if [ -f "$dir" ]; then
-    echo "$dir is a file."
-    return
-  fi
+    dir="$1"
+    if [ -f "$dir" ]; then
+        echo "$dir is a file."
+        return
+    fi
 
-  if [ -d "$dir" ]; then
-    for subDir in $dir/*; do
-      __QUICK_ACCESS_DIRECTORY+=$subDir;
-    done
-  else
-    echo "$dir is not exist."
-  fi
+    if [ -d "$dir" ]; then
+        for subDir in $dir/*; do
+            __QUICK_ACCESS_DIRECTORY+=$subDir;
+        done
+    else
+        echo "$dir is not exist."
+    fi
 }
 
-function __directory_get_path_list_from_quick_access {
+function __directory_list_quick_access {
+    print -l $__QUICK_ACCESS_DIRECTORY
+}
+
+function __directory_search_path_list_from_quick_access {
     search=${1///*};
     echo $search
     print -l $__QUICK_ACCESS_DIRECTORY | while read line
     do
-      if [[ $line == *$search* ]]; then
-        echo $line;
-      fi
+        if [[ $line == *$search* ]]; then
+            echo $line;
+        fi
     done
 }
 
