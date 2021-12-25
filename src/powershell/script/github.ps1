f0389193f2b4d9e811c3f901c5846bfba711b95f
lnh <lnhdyx@outlook.com>
2021-12-25 21:44:19 +0800

    Update github.ps1
    --ci

--- src/powershell/script/github.ps1
+++ src/powershell/script/github.ps1
@@ -1,9 +1,11 @@
+git clone https://github.com/linianhui/benchmark-http.git
+git clone https://github.com/linianhui/benchmark-nginx.git
 git clone https://github.com/linianhui/blog.git
 git clone https://github.com/linianhui/channel9-downloader.git
 git clone https://github.com/linianhui/chrome-extensions.git
 git clone https://github.com/linianhui/cnblogs-theme.git
-git clone https://github.com/linianhui/code-guide.git
 git clone https://github.com/linianhui/code.git
+git clone https://github.com/linianhui/code-guide.git
 git clone https://github.com/linianhui/div.git
 git clone https://github.com/linianhui/docker.git
 git clone https://github.com/linianhui/example-aspnetcore.git
@@ -16,8 +18,7 @@ git clone https://github.com/linianhui/example-spring.git
 git clone https://github.com/linianhui/git-web.git
 git clone https://github.com/linianhui/I-love-mommy.git
 git clone https://github.com/linianhui/jvm.git
-git clone https://github.com/linianhui/http-benchmark.git
 git clone https://github.com/linianhui/linianhui.github.io.git
-git clone https://github.com/linianhui/networking-programming.git
 git clone https://github.com/linianhui/networking.git
+git clone https://github.com/linianhui/networking-programming.git
 git clone https://github.com/linianhui/nuget-server.git
