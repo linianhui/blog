cdee6332b81dd01ee3e63281ec87229d3cb72e18
lnh <lnhdyx@outlook.com>
2022-05-04 11:11:25 +0800

    Update file.ps1
    --ci

--- src/powershell/script/file.ps1
+++ src/powershell/script/file.ps1
@@ -7,11 +7,41 @@ function File-GetBigFiles ([int]$top = 20) {
     Log-Debug "begin..." $begin
 
     Get-ChildItem -Path $(Get-Location) -File -Recurse |
-        Sort-Object Length -Descending |
-        Select-Object -First $top |
-        Format-Table -Property @{Label = "Size"; Expression = {($_.Length / 1MB).ToString(('0.000')) + 'MB'} }, FullName -Wrap
+    Sort-Object Length -Descending |
+    Select-Object -First $top |
+    Format-Table -Property @{Label = "Size"; Expression = { ($_.Length / 1MB).ToString(('0.000')) + 'MB' } }, FullName -Wrap
 
     $end = Get-Date
     Log-Debug "end..." $end
     Log-Debug "elapsed times" ($end - $begin)
 }
+
+
+function File-GetAll {
+    param (
+        [string] $Path = $(Get-Location)
+    )
+
+    Get-ChildItem -Path $Path -Directory | ForEach-Object {
+        File-GetAll -Path $_
+    }
+
+    Get-ChildItem -Path $Path -File | ForEach-Object {
+        $_.FullName
+    }
+}
+
+function File-Diff {
+    param (
+        [string] $PartFile,
+        [string] $FullFile
+    )
+
+    $PartContent = $(Get-Content $PartFile)
+    Get-Content -Path $FullFile | ForEach-Object {
+        if ($PartContent.Contains($_) -eq $FALSE) {
+            $_
+        }
+    }
+}
+
