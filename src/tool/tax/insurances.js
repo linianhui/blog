3ab223fd6fe1554d1f3c8ed817d956c2cbb23a33
lnh <lnhdyx@outlook.com>
2022-08-06 13:26:42 +0800

    add beijing 2022 insurance
    --ci

--- src/tool/tax/insurances.js
+++ src/tool/tax/insurances.js
@@ -1,4 +1,42 @@
 const insurances = {
+    "北京2022社保基数": {
+        养老保险: {
+            min: 5869,
+            max: 31884,
+            corporationPercentage: 16,
+            personalPercentage: 8
+        },
+        医疗保险: {
+            min: 5869,
+            max: 31884,
+            corporationPercentage: 9,
+            personalPercentage: 2
+        },
+        失业保险: {
+            min: 5869,
+            max: 31884,
+            corporationPercentage: 0.5,
+            personalPercentage: 0.5
+        },
+        工伤保险: {
+            min: 5869,
+            max: 31884,
+            corporationPercentage: 0.4,
+            personalPercentage: 0
+        },
+        生育保险: {
+            min: 5869,
+            max: 31884,
+            corporationPercentage: 0.8,
+            personalPercentage: 0
+        },
+        公积金: {
+            min: 0,
+            max: 31884,
+            corporationPercentage: 12,
+            personalPercentage: 12
+        }
+    },
     "北京2021社保基数": {
         养老保险: {
             min: 5360,
