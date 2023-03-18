a6a5b5010db76246bbaf7c43b65ed7f347eefe8d
lnh <lnhdyx@outlook.com>
2023-03-18 17:50:20 +0800

    add Java-Set-Jdk
    --ci

--- src/powershell/script/java.ps1
+++ src/powershell/script/java.ps1
@@ -1,6 +1,30 @@
-function Java-Arthas()
-{
+function Java-Arthas() {
     # https://arthas.gitee.io/install-detail.html
     Log-Debug "java -jar $ENV:JAVA_HOME\arthas-boot.jar --repo-mirror aliyun --use-http $ARGS"
     java -jar $ENV:JAVA_HOME\arthas-boot.jar --repo-mirror aliyun --use-http $ARGS
+}
+
+function Java-Set-Jdk {
+    param (
+        [ValidateSet('8', '11', '17', '19',"12")]
+        [string] $Version
+    )
+    $BasePath = 'd:\_app\_java'
+    $JdkPath = $BasePath + $Version
+
+    $CurrentJdkPath = (Get-Item -Path $BasePath).Target
+    Log-Info "current jdk path : $CurrentJdkPath"
+
+    if ($JdkPath.Equals($CurrentJdkPath)) {
+        Log-Warn "not need change"
+    }
+    else {
+        Log-Warn "Remove-Item $BasePath -Force -Confirm:$False"
+        Remove-Item $BasePath -Force -Confirm:$False
+
+        Log-Info "New-Item -ItemType Junction -Path $BasePath -Target $JdkPath | Out-Null"
+        New-Item -ItemType Junction -Path $BasePath -Target $JdkPath | Out-Null
+    }
+
+    Log-Debug "now jdk path : $((Get-Item -Path $BasePath).Target)"
 }
