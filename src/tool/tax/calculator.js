6972f83b09b72561342c5e969a0865d182acf986
lnh <lnhdyx@outlook.com>
2023-03-26 13:04:14 +0800

    refactor tax
    --ci

--- src/tool/tax/calculator.js
+++ src/tool/tax/calculator.js
@@ -7,8 +7,8 @@ function buildSalaryList(amount, month, insurances, rates) {
             month: index,
             base: amount,
             exempted: rates.exempted,
-            rates: JSON.parse(JSON.stringify(rates)),
-            insurances: JSON.parse(JSON.stringify(insurances)),
+            rates: deepClone(rates),
+            insurances: deepClone(insurances),
         };
     }
     if (month > 12) {
@@ -17,30 +17,23 @@ function buildSalaryList(amount, month, insurances, rates) {
             base: amount * (month - 12),
             months: (month - 12),
             exempted: 0,
-            rates: JSON.parse(JSON.stringify(rates)),
-            insurances: JSON.parse(JSON.stringify(insurances)),
+            rates: deepClone(rates),
+            insurances: deepClone(insurances),
         };
     }
     return salaryList;
 }
 
 function buildSummary(salaryList) {
-    const month = salaryList.length;
     const last = salaryList[salaryList.length - 1];
-    const 医疗保险YTD = last.insurances.医疗保险.personalYTD;
-    const 公积金YTD = round2(last.insurances.公积金.personalYTD + last.insurances.公积金.corporationYTD);
-    const totalActualYTD = round2(医疗保险YTD + 公积金YTD + last.actualYTD);
+    const 公积金SumYTD = findInsurance(last.insurances.items, "公积金").sumYTD;
+    const totalActualYTD = round2(公积金SumYTD + last.actualYTD);
     return {
-        base: last.base || 0,
-        baseYTD: last.baseYTD,
-        医疗保险YTD: 医疗保险YTD,
-        公积金YTD: 公积金YTD,
-        taxableYTD: last.taxableYTD,
-        taxYTD: last.taxYTD,
-        actualAvg: round2(last.actualYTD / month),
-        actualYTD: last.actualYTD,
+        last: last,
+        公积金SumYTD: 公积金SumYTD,
         totalActualYTD: totalActualYTD,
-        diffYTD: round2(last.baseYTD - totalActualYTD)
+        personalDiffYTD: round2(last.baseYTD - totalActualYTD),
+        corporationDiffYTD: round2(last.corporationYTD - totalActualYTD),
     };
 }
 
@@ -59,6 +52,7 @@ function calculateOneMonth(salary, salaryOfPrevMonth) {
     taxable();
     tax();
     actual();
+    corporation();
 
     function base() {
         salary.baseYTD = round2((salary.base || 0) + (salaryOfPrevMonth.baseYTD || 0));
@@ -69,42 +63,41 @@ function calculateOneMonth(salary, salaryOfPrevMonth) {
     }
 
     function insurances() {
-        for (let key in salary.insurances) {
-            const insurance = salary.insurances[key];
-            const insuranceOfPrevMonth = salaryOfPrevMonth.insurances[key];
-            const base = Math.min(Math.max(insurance.min, salary.base), insurance.max);
+        for (let index in salary.insurances.items) {
+            const insurance = salary.insurances.items[index];
+            const insuranceOfPrevMonth = findInsurance(salaryOfPrevMonth.insurances.items, insurance.name);
+
+            insurance.base = Math.min(Math.max(insurance.min, salary.base), insurance.max);
+
             if (salary.month <= 12) {
-                insurance.personal = round2((base * insurance.personalPercentage / 100));
+                insurance.personal = round2((insurance.base * insurance.personalPercentage / 100));
             } else {
                 insurance.personal = 0;
             }
             insurance.personalYTD = round2((insurance.personal || 0) + (insuranceOfPrevMonth.personalYTD || 0));
+
             if (salary.month <= 12) {
-                insurance.corporation = round2((base * insurance.corporationPercentage / 100));
+                insurance.corporation = round2((insurance.base * insurance.corporationPercentage / 100));
             } else {
                 insurance.corporation = 0;
             }
             insurance.corporationYTD = round2((insurance.corporation || 0) + (insuranceOfPrevMonth.corporationYTD || 0));
+
+            insurance.sum = round2((insurance.personal || 0) + (insurance.corporation || 0));
+            insurance.sumYTD = round2((insurance.sum || 0) + (insuranceOfPrevMonth.sumYTD || 0));
         }
 
-        salary.insurancesPersonal = sum(x => x.personal);
-        salary.insurancesPersonalYTD = sum(x => x.personalYTD);
-        salary.insurancesCorporation = sum(x => x.corporation);
-        salary.insurancesCorporationYTD = sum(x => x.corporationYTD);
-
-        function sum(func) {
-            var total = 0;
-            for (let key in salary.insurances) {
-                const value = salary.insurances[key];
-                total = round2(total + func(value));
-            }
-            return total;
-        }
+        salary.insurances.personal = sum(salary.insurances.items, x => x.personal);
+        salary.insurances.personalYTD = round2((salary.insurances.personal || 0) + (salaryOfPrevMonth.insurances.personal || 0));
+        salary.insurances.corporation = sum(salary.insurances.items, x => x.corporation);
+        salary.insurances.corporationYTD = round2((salary.insurances.corporation || 0) + (salaryOfPrevMonth.insurances.corporationYTD || 0));
+        salary.insurances.sum = sum(salary.insurances.items, x => x.sum);
+        salary.insurances.sumYTD = round2((salary.insurances.sum || 0) + (salaryOfPrevMonth.insurances.sumYTD || 0));
     }
 
     function taxable() {
-        salary.taxable = Math.max(0, round2(salary.base - salary.insurancesPersonal - salary.exempted));
-        salary.taxableYTD = Math.max(0, round2(salary.baseYTD - salary.insurancesPersonalYTD - salary.exemptedYTD));
+        salary.taxable = Math.max(0, round2(salary.base - salary.insurances.personal - salary.exempted));
+        salary.taxableYTD = Math.max(0, round2(salary.baseYTD - salary.insurances.personalYTD - salary.exemptedYTD));
     }
 
     function tax() {
@@ -138,13 +131,44 @@ function calculateOneMonth(salary, salaryOfPrevMonth) {
         }
     }
 
-
     function actual() {
-        salary.actual = round2((salary.base || 0) - (salary.insurancesPersonal || 0) - (salary.tax || 0));
+        salary.actual = round2((salary.base || 0) - (salary.insurances.personal || 0) - (salary.tax || 0));
         salary.actualYTD = round2((salary.actual || 0) + (salaryOfPrevMonth.actualYTD || 0));
+    }
+
+    function corporation() {
+        salary.corporation = round2((salary.base || 0) + (salary.insurances.corporation || 0));
+        salary.corporationYTD = round2((salary.corporation || 0) + (salaryOfPrevMonth.corporationYTD || 0));
     }
 }
 
-function round2(num) {
-    return Math.round(num * 100) / 100;
+function round2(value) {
+    const number = value || 0;
+    return Math.round(number * 100) / 100;
+}
+
+function findInsurance(items, name) {
+    for (let index in items) {
+        var item = items[index];
+        if (item.name == name) {
+            return item;
+        }
+    }
+}
+
+function sum(items, func) {
+    var result = 0;
+    for (let index in items) {
+        const item = items[index];
+        result = round2(result + func(item));
+    }
+    return round2(result);
+}
+
+function deepClone(value) {
+    return JSON.parse(JSON.stringify(value));
+}
+
+function log(type, value) {
+    console.log(type, deepClone(value));
 }
--- src/tool/tax/insurances.js
+++ src/tool/tax/insurances.js
@@ -1,116 +1,146 @@
 var insurances = {
     "北京2022社保基数": {
-        养老保险: {
-            min: 5869,
-            max: 31884,
-            corporationPercentage: 16,
-            personalPercentage: 8
-        },
-        医疗保险: {
-            min: 5869,
-            max: 31884,
-            corporationPercentage: 9,
-            personalPercentage: 2
-        },
-        失业保险: {
-            min: 5869,
-            max: 31884,
-            corporationPercentage: 0.5,
-            personalPercentage: 0.5
-        },
-        工伤保险: {
-            min: 5869,
-            max: 31884,
-            corporationPercentage: 0.4,
-            personalPercentage: 0
-        },
-        生育保险: {
-            min: 5869,
-            max: 31884,
-            corporationPercentage: 0.8,
-            personalPercentage: 0
-        },
-        公积金: {
-            min: 0,
-            max: 31884,
-            corporationPercentage: 12,
-            personalPercentage: 12
-        }
+        min: 5869,
+        max: 31884,
+        items: [
+            {
+                name: "养老保险",
+                min: 5869,
+                max: 31884,
+                corporationPercentage: 16,
+                personalPercentage: 8
+            },
+            {
+                name: "医疗保险",
+                min: 5869,
+                max: 31884,
+                corporationPercentage: 9,
+                personalPercentage: 2
+            },
+            {
+                name: "失业保险",
+                min: 5869,
+                max: 31884,
+                corporationPercentage: 0.5,
+                personalPercentage: 0.5
+            },
+            {
+                name: "工伤保险",
+                min: 5869,
+                max: 31884,
+                corporationPercentage: 0.4,
+                personalPercentage: 0
+            },
+            {
+                name: "生育保险",
+                min: 5869,
+                max: 31884,
+                corporationPercentage: 0.8,
+                personalPercentage: 0
+            },
+            {
+                name: "公积金",
+                min: 0,
+                max: 31884,
+                corporationPercentage: 12,
+                personalPercentage: 12
+            }
+        ]
     },
     "北京2021社保基数": {
-        养老保险: {
-            min: 5360,
-            max: 28221,
-            corporationPercentage: 16,
-            personalPercentage: 8
-        },
-        医疗保险: {
-            min: 5360,
-            max: 28221,
-            corporationPercentage: 9,
-            personalPercentage: 2
-        },
-        失业保险: {
-            min: 5360,
-            max: 28221,
-            corporationPercentage: 0.5,
-            personalPercentage: 0.5
-        },
-        工伤保险: {
-            min: 5360,
-            max: 28221,
-            corporationPercentage: 0.4,
-            personalPercentage: 0
-        },
-        生育保险: {
-            min: 5360,
-            max: 28221,
-            corporationPercentage: 0.8,
-            personalPercentage: 0
-        },
-        公积金: {
-            min: 0,
-            max: 25401,
-            corporationPercentage: 12,
-            personalPercentage: 12
-        }
+        min: 5360,
+        max: 28221,
+        items: [
+            {
+                name: "养老保险",
+                min: 5360,
+                max: 28221,
+                corporationPercentage: 16,
+                personalPercentage: 8
+            },
+            {
+                name: "医疗保险",
+                min: 5360,
+                max: 28221,
+                corporationPercentage: 9,
+                personalPercentage: 2
+            },
+            {
+                name: "失业保险",
+                min: 5360,
+                max: 28221,
+                corporationPercentage: 0.5,
+                personalPercentage: 0.5
+            },
+            {
+                name: "工伤保险",
+                min: 5360,
+                max: 28221,
+                corporationPercentage: 0.4,
+                personalPercentage: 0
+            },
+            {
+                name: "生育保险",
+                min: 5360,
+                max: 28221,
+                corporationPercentage: 0.8,
+                personalPercentage: 0
+            },
+            {
+                name: "公积金",
+                min: 0,
+                max: 25401,
+                corporationPercentage: 12,
+                personalPercentage: 12
+            }
+        ]
     },
     "杭州2021社保基数": {
-        养老保险: {
-            min: 3321.6,
-            max: 17880.75,
-            corporationPercentage: 14,
-            personalPercentage: 8
-        },
-        医疗保险: {
-            min: 3321.6,
-            max: 17880.75,
-            corporationPercentage: 10.5,
-            personalPercentage: 2
-        },
-        失业保险: {
-            min: 3321.6,
-            max: 17880.75,
-            corporationPercentage: 0.5,
-            personalPercentage: 0.5
-        },
-        工伤保险: {
-            min: 3321.6,
-            max: 17880.75,
-            corporationPercentage: 0.4,
-            personalPercentage: 0
-        },
-        生育保险: {
-            min: 3321.6,
-            max: 17880.75,
-            corporationPercentage: 1.2,
-            personalPercentage: 0
-        },
-        公积金: {
-            min: 0,
-            max: 17880.75,
-            corporationPercentage: 12,
-            personalPercentage: 12
-        }
+        min: 3321.6,
+        max: 17880.75,
+        items: [
+            {
+                name: "养老保险",
+                min: 3321.6,
+                max: 17880.75,
+                corporationPercentage: 14,
+                personalPercentage: 8
+            },
+            {
+                name: "医疗保险",
+                min: 3321.6,
+                max: 17880.75,
+                corporationPercentage: 10.5,
+                personalPercentage: 2
+            },
+            {
+                name: "失业保险",
+                min: 3321.6,
+                max: 17880.75,
+                corporationPercentage: 0.5,
+                personalPercentage: 0.5
+            },
+            {
+                name: "工伤保险",
+                min: 3321.6,
+                max: 17880.75,
+                corporationPercentage: 0.4,
+                personalPercentage: 0
+            },
+            {
+                name: "生育保险",
+                min: 3321.6,
+                max: 17880.75,
+                corporationPercentage: 1.2,
+                personalPercentage: 0
+            },
+            {
+                name: "公积金",
+                min: 0,
+                max: 17880.75,
+                corporationPercentage: 12,
+                personalPercentage: 12
+            }
+        ]
     }
 };
