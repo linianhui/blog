cea3b9d7422eb9a75f1a9ac9cf84aff7c79c5009
lnh <lnhdyx@outlook.com>
2023-09-10 00:03:48 +0800

    Update prompt.ps1
    --ci

--- src/powershell/script/prompt.ps1
+++ src/powershell/script/prompt.ps1
@@ -7,18 +7,20 @@ function prompt () {
     $OLD_LASTEXITCODE = $LASTEXITCODE
 
     $Now = [System.DateTimeOffset]::Now
-    $CurrentYearLastDay = New-Object -TypeName System.DateOnly -ArgumentList ($Now.Year,12,31)
+    $CurrentYearLastDay = New-Object -TypeName System.DateOnly -ArgumentList ($Now.Year, 12, 31)
     $DateTime = $Now.ToString('yy-MM-dd HH:mm:ss K')
     $DayOfWeek = [System.Int32]$Now.DayOfWeek
     $Week = $DayOfWeek.Equals(0)?7:$DayOfWeek
     $DayOfYear = $Now.DayOfYear
+    $WeekOfYear = [System.Math]::Round(($DayOfYear + 7) / 7)
+    $ResidueWeekOfYear = [System.Math]::Round(($CurrentYearLastDay.DayOfYear - $DayOfYear) / 7)
     $DayOfNewYear = $CurrentYearLastDay.DayOfYear - $DayOfYear
     $UnixTimeMilliseconds = $Now.ToUnixTimeMilliseconds()
     $UserPrompt = UI-GetUserPrompt
     $UserPromptPrefix = $UserPrompt.Prefix
     $UserPromptText = $UserPrompt.Text
 
-    Write-Host -NoNewline "`n$UserPromptText $DateTime $UnixTimeMilliseconds w$Week d$DayOfYear-$DayOfNewYear" -ForegroundColor Gray
+    Write-Host -NoNewline "`n$UserPromptText $DateTime $UnixTimeMilliseconds w$Week-$WeekOfYear-$ResidueWeekOfYear d$DayOfYear-$DayOfNewYear" -ForegroundColor Gray
     if ($OLD_LASTEXITCODE -gt 0) {
         Write-Host -NoNewline " $OLD_LASTEXITCODE" -ForegroundColor Red
     }
