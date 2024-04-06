6dfd81dfe8ebe505c3046655a3903ae150301fbb
lnh <lnhdyx@outlook.com>
2024-04-06 19:14:44 +0800

    refactor loan
    --ci

--- src/tool/loan/calculator.js
+++ src/tool/loan/calculator.js
@@ -1,19 +1,50 @@
-function calculateMonths(principal, month, rate, date, aheadDate, aheadAmount) {
-    var defaultResult = calculateMonthsCore(principal, month, rate, date);
+function calculate(principal, month, rate, date, aheadAmount) {
+    var result = calculateCore(principal, month, rate, date);
+    var ahead = calculateAhead(result, principal, month, rate, date, aheadAmount);
+    console.log("a",ahead);
+    return tryMerge(result, ahead);
+}
+
+function tryMerge(result, ahead) {
+    if (ahead) {
+        var oldSum = blog.deepClone(result.sum);
+        result.sum = ahead.sum;
+        result.sum.ahead = diffSum(oldSum, ahead.sum);
+        result.sum.ahead.month = ahead.months.length - result.months.length;
+        for (let index = 0; index < result.months.length; index++) {
+            var oldMonth = result.months[index];
+            var newMonth = ahead.months[index];
+            result.months[index] = tryMergeMonth(oldMonth, newMonth);
+        }
+    }
+
+    return result;
+}
+
+function tryMergeMonth(old, ahead) {
+    ahead = ahead || defaultMonth();
+
+    var oldMonth = blog.deepClone(old);
+    old.avgPrincipal = ahead.avgPrincipal;
+    old.avgInterest = ahead.avgInterest;
+    old.ahead = diffMonth(oldMonth, ahead);
+
+    return old;
+}
+
+function calculateAhead(defaultResult, principal, month, rate, date, aheadAmount) {
     if (aheadAmount >= 10000) {
-        var aheads = calculateAhead(principal, month, rate, date, aheadAmount);
+        var aheads = tryCalculateAheadAll(principal, month, rate, date, aheadAmount);
         for (var i = 0; i < aheads.length - 1; i++) {
             var aheadCurrent = aheads[i];
             var aheadNext = aheads[i + 1];
             if (inRange(defaultResult, aheadCurrent, aheadNext)) {
-                defaultResult.ahead = aheadCurrent;
-                break;
+                return aheadCurrent;
             }
         }
     }
-    return defaultResult;
+    return null;
 }
-
 
 function inRange(defaultResult, aheadCurrent, aheadNext) {
     var defaultAmount = defaultResult.months[0].avgPrincipal.principal;
@@ -22,41 +53,67 @@ function inRange(defaultResult, aheadCurrent, aheadNext) {
     return aheadCurrentAmount < defaultAmount && defaultAmount <= aheadNextAmount;
 }
 
-function calculateAhead(principal, month, rate, date, aheadAmount) {
+function tryCalculateAheadAll(principal, month, rate, date, aheadAmount) {
     var residuePrincipal = blog.round2(principal - aheadAmount);
     var result = [];
     for (var i = month; i > 0; i--) {
-        var currentResult = calculateMonthsCore(residuePrincipal, i, rate, date);
-        result.push(currentResult);
+        result.push(calculateCore(residuePrincipal, i, rate, date));
     }
     return result;
 }
 
-function calculateMonthsCore(principal, month, rate, date) {
+function calculateCore(principal, month, rate, date) {
     var rateMonth = rate / 12 / 100;
-    var months = avgPrincipalAndAvgInterest(principal, month, rateMonth, date);
-    var avgPrincipalTotalInterest = blog.sum(months, x => x.avgPrincipal.interest);
-    var avgInterestTotalInterest = blog.sum(months, x => x.avgInterest.interest);
+    var months = calculateMonths(principal, month, rateMonth, date);
+
+    var avgPrincipalSum = calculateSum(principal, months, x => x.avgPrincipal.interest);
+    var avgInterestSum = calculateSum(principal, months, x => x.avgInterest.interest);
+
     return {
-        avgPrincipalTotal: blog.round2(principal + avgPrincipalTotalInterest),
-        avgPrincipalTotalInterest: avgPrincipalTotalInterest,
-        avgInterestTotal: blog.round2(principal + avgInterestTotalInterest),
-        avgInterestTotalInterest: avgInterestTotalInterest,
-        diff: blog.round2(avgInterestTotalInterest - avgPrincipalTotalInterest),
-        months: months
+        months: months,
+        sum: {
+            avgPrincipal: avgPrincipalSum,
+            avgInterest: avgInterestSum,
+            interestDiff: blog.round2(avgInterestSum.interest - avgPrincipalSum.interest),
+        }
     };
 }
 
-function avgPrincipalAndAvgInterest(principal, month, rateMonth, date) {
-    var avgPrincipalMonths = avgPrincipal(principal, month, rateMonth);
-    var avgInterestMonths = avgInterest(principal, month, rateMonth);
+function diffSum(old, ahead) {
+    var avgPrincipal = diffSumItem(old.avgPrincipal, ahead.avgPrincipal);
+    var avgInterest = diffSumItem(old.avgInterest, ahead.avgInterest);
+    return {
+        avgPrincipal: avgPrincipal,
+        avgInterest: avgInterest,
+        interestDiff: blog.round2(avgInterest.interest - avgPrincipal.interest),
+    }
+}
 
+function calculateSum(principal, months, func) {
+    var sum = blog.sum(months, func);
+    return {
+        principal: principal,
+        interest: sum,
+        total: blog.round2(principal + sum),
+    };
+}
+
+function diffSumItem(old, ahead) {
+    return {
+        principal: blog.round2(ahead.principal - old.principal),
+        interest: blog.round2(ahead.interest - old.interest),
+        total: blog.round2(ahead.total - old.total),
+    };
+}
+
+function calculateMonths(principal, month, rateMonth, date) {
     var result = [];
+    var avgPrincipalMonths = calculateAvgPrincipalMonths(principal, month, rateMonth);
+    var avgInterestMonths = calculateAvgInterestMonths(principal, month, rateMonth);
 
     for (let index = 0; index < month; index++) {
         var avgPrincipalMonth = avgPrincipalMonths[index];
         var avgInterestMonth = avgInterestMonths[index];
-
         result.push({
             month: addMonth(date, index),
             avgPrincipal: avgPrincipalMonth,
@@ -67,8 +124,24 @@ function avgPrincipalAndAvgInterest(principal, month, rateMonth, date) {
     return result;
 }
 
+function diffMonth(old, ahead) {
+    var avgPrincipal = diffMonthItem(old.avgPrincipal, ahead.avgPrincipal);
+    var avgInterest = diffMonthItem(old.avgInterest, ahead.avgInterest);
+    return {
+        avgPrincipal: avgPrincipal,
+        avgInterest: avgInterest
+    };
+}
+
+function defaultMonth() {
+    return {
+        avgPrincipal: defaultMonthItem(),
+        avgInterest: defaultMonthItem()
+    };
+}
+
 // 等额本金
-function avgPrincipal(principal, month, rateMonth) {
+function calculateAvgPrincipalMonths(principal, month, rateMonth) {
     var result = [];
 
     // 每月本金
@@ -96,8 +169,26 @@ function avgPrincipal(principal, month, rateMonth) {
     return result;
 }
 
+function diffMonthItem(old, ahead) {
+    return {
+        amount: blog.round2(ahead.amount - old.amount),
+        principal: blog.round2(ahead.principal - old.principal),
+        interest: blog.round2(ahead.interest - old.interest),
+        principalBalance: blog.round2(ahead.principalBalance - old.principalBalance)
+    }
+}
+
+function defaultMonthItem() {
+    return {
+        amount: 0,
+        principal: 0,
+        interest: 0,
+        principalBalance: 0
+    }
+}
+
 // 等额本息
-function avgInterest(principal, month, rateMonth) {
+function calculateAvgInterestMonths(principal, month, rateMonth) {
     var result = [];
 
     // 每月还款总金额
@@ -130,11 +221,10 @@ function addMonth(date, month) {
     var newDate = new Date(date);
     newDate.setMonth(date.getMonth() + month);
     return newDate.getFullYear()
-        + "年"
+        + "-"
         + fill0(newDate.getMonth() + 1)
-        + "月"
-        + fill0(newDate.getDate())
-        + "日";
+        + "-"
+        + fill0(newDate.getDate());
 }
 
 function fill0(value) {
