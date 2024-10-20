dcd9ccde3d3a0a7276f172ce4ca8104ac0b1cd89
lnh <lnhdyx@outlook.com>
2024-10-20 10:31:12 +0800

    Update dism.ps1
    --ci

--- src/powershell/script/dism.ps1
+++ src/powershell/script/dism.ps1
@@ -5,16 +5,49 @@
 
 function Dism-Backup-Os {
     param (
-        [string] $SourceDriver = 'c',
-        [string] $TargetDriver = 'd'
+        [string] $SourcePath = 'c:\',
+        [string] $TargetPath = 'f:\_backup\'
     )
 
-    [string]$OsName= Dism-Get-OsName;
+    [string]$OsName = Dism-Get-OsName;
     [string]$Date = $(Get-Date -Format 'yyyy-MM-dd');
     [string]$Name = "${OsName}-${Date}".ToLower();
-    $COMMAND = "DISM /Capture-Image /CaptureDir:${SourceDriver}:\ /Name:${Name} /ImageFile:${TargetDriver}:\${Name}.wim";
-    Log-Debug "$COMMAND";
-    $COMMAND | Out-File -FilePath "${TargetDriver}:\dism-${Name}.bat"
+    [string]$ImageFile = "${TargetPath}${OsName}.wim";
+
+    $CaptureImageCommand = "DISM /Capture-Image /CaptureDir:${SourcePath} /Name:${Name} /ImageFile:${ImageFile}";
+    $GetImageCommand = "DISM /Get-ImageInfo /ImageFile:$ImageFile";
+    $BatFilePath = "d:\dism-${OsName}.bat";
+
+    $CaptureImageCommand | Out-File -FilePath $BatFilePath
+    $GetImageCommand | Out-File -Append -FilePath $BatFilePath
+    Get-Content -Path $BatFilePath
+}
+
+function Dism-Backup-Os-Append {
+    param (
+        [string] $SourcePath = 'c:\',
+        [string] $ImageFile
+    )
+    $GetImageCommand = "DISM /Get-ImageInfo /ImageFile:$ImageFile";
+
+    [string]$OsName = Dism-Get-OsName;
+    [string]$Date = $(Get-Date -Format 'yyyy-MM-dd');
+    [string]$Name = "${OsName}-${Date}".ToLower();
+
+    $AppendCommand = "DISM /Append-Image /CaptureDir:${SourcePath} /Name:${Name} /ImageFile:$ImageFile";
+    $BatFilePath = "d:\dism-${OsName}-append.bat";
+
+    $GetImageCommand | Out-File -FilePath $BatFilePath
+    $AppendCommand | Out-File -Append -FilePath $BatFilePath
+    $GetImageCommand | Out-File -Append -FilePath $BatFilePath
+    Get-Content -Path $BatFilePath
+}
+
+function Dism-Get-Image-Info {
+    param (
+        [string] $ImageFile
+    )
+    DISM /Get-ImageInfo /ImageFile:$ImageFile
 }
 
 function Dism-Get-Intl {
@@ -30,6 +63,6 @@ function Dism-Get-Lang {
 function Dism-Get-OsName {
     $ComputerInfo = Get-ComputerInfo;
     [string]$CsName = $ComputerInfo.CsName.Trim().ToLower();
-    [string]$OsName = $($($ComputerInfo.OsName.ToLower() -Replace 'microsoft','').Trim() -Replace ' ', '-');
+    [string]$OsName = $($($ComputerInfo.OsName.ToLower() -Replace 'microsoft', '').Trim() -Replace ' ', '-');
     return "${CsName}-${OsName}";
 }
