7445a37c3ede4a6eaffe6a312a981705a98c5759
lnh <lnhdyx@outlook.com>
2025-06-28 23:55:33 +0800

    Update enable-hyper-v.cmd
    --ci

--- src/powershell/hyper-v/enable-hyper-v.cmd
+++ src/powershell/hyper-v/enable-hyper-v.cmd
@@ -1,6 +1,7 @@
 @ECHO ON
-@REM 家庭版启用Hyper-V
+REM 家庭版启用Hyper-V
 SET PACKAGE_PATH=%SystemRoot%\servicing\Packages\
 FOR /F %%i IN ('DIR /B %PACKAGE_PATH%*Hyper-V*.mum') DO DISM /Online /NoRestart /Add-Package:"%PACKAGE_PATH%%%i"
 DISM /Online /Enable-Feature /FeatureName:Microsoft-Hyper-V -All /LimitAccess /ALL
+@ECHO OFF
 PAUSE
