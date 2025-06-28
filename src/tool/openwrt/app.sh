936712d5b9c4aa32e9a49997f73a344c5fab2219
lnh <lnhdyx@outlook.com>
2025-06-28 08:03:32 +0800

    Update app.sh
    --ci

--- src/tool/openwrt/app.sh
+++ src/tool/openwrt/app.sh
@@ -1,8 +1,9 @@
 opkg update
 
-opkg install luci-app-acl
-opkg install luci-app-commands
+#opkg install luci-app-acl
+#opkg install luci-app-commands
 opkg install luci-app-watchcat
-opkg install luci-app-cshark
-opkg install luci-app-netdata
-opkg install luci-app-natmap
+#opkg install luci-app-cshark
+opkg install tcpdump
+##opkg install luci-app-netdata
+#opkg install luci-app-natmap
