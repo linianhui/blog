1df3aeca51ccdc44b7be2886235fb4008aae6c52
lnh <lnhdyx@outlook.com>
2025-07-05 11:54:15 +0800

    Update http.ps1
    --ci

--- src/powershell/script/http.ps1
+++ src/powershell/script/http.ps1
@@ -1,14 +1,44 @@
 
 # https://github.com/svenstaro/miniserve
 function Http-Server() {
+    param (
+        [ValidateSet('dufs', 'miniserve')]
+        [string] $App = 'miniserve'
+    )
+
+    if ($App -eq 'miniserve') {
+        Http-Server-Mineserve $Args
+    }
+
+    if ($App -eq 'dufs') {
+        Http-Server-Dufs $Args
+    }
+}
+
+# https://github.com/svenstaro/miniserve
+function Http-Server-Mineserve() {
     Log-Debug "miniserve --qrcode --upload-files --dirs-first --hide-theme-selector --enable-webdav --interfaces 192.168.2.2 $Args"
     miniserve --qrcode --upload-files --dirs-first --hide-theme-selector --enable-webdav --interfaces 192.168.2.2 $Args
 }
 
+# https://github.com/sigoden/dufs
+function Http-Server-Dufs() {
+    Log-Debug "dufs --bind 192.168.2.2 --port 8080 --allow-all $Args"
+    dufs --bind 192.168.2.2 --port 8080 --allow-all $Args
+}
+
 function Http-Server-Temp() {
-    Http-Server 'z:/__temp'
+    param (
+        [ValidateSet('dufs', 'miniserve')]
+        [string] $App = 'miniserve'
+    )
+    Http-Server -App $App 'z:/__temp'
 }
 
 function Http-Server-Share() {
-    Http-Server 'z:/_share'
+    param (
+        [ValidateSet('dufs', 'miniserve')]
+        [string] $App = 'miniserve'
+    )
+    Http-Server -App $App 'z:/_share'
 }
