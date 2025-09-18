7d621554531824252f92c76a7fef696933972c1b
lnh <lnhdyx@outlook.com>
2025-09-18 22:45:20 +0800

    Update insurances.js
    --ci

--- src/tool/tax/insurances.js
+++ src/tool/tax/insurances.js
@@ -1,4 +1,52 @@
 var insurances = {
+    "北京2025社保基数": {
+        min: 7162,
+        max: 35811,
+        items: [
+            {
+                name: "养老保险",
+                min: 7162,
+                max: 35811,
+                corporationPercentage: 16,
+                personalPercentage: 8
+            },
+            {
+                name: "医疗保险",
+                min: 7162,
+                max: 35811,
+                corporationPercentage: 9.8,
+                personalPercentage: 2
+            },
+            {
+                name: "失业保险",
+                min: 7162,
+                max: 35811,
+                corporationPercentage: 0.5,
+                personalPercentage: 0.5
+            },
+            {
+                name: "工伤保险",
+                min: 7162,
+                max: 35811,
+                corporationPercentage: 0.4,
+                personalPercentage: 0
+            },
+            {
+                name: "生育保险",
+                min: 7162,
+                max: 35811,
+                corporationPercentage: 0.8,
+                personalPercentage: 0
+            },
+            {
+                name: "公积金",
+                min: 0,
+                max: 35811,
+                corporationPercentage: 12,
+                personalPercentage: 12
+            }
+        ]
+    },
     "北京2024社保基数": {
         min: 6821,
         max: 35283,
