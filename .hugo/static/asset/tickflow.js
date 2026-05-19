3b93be2ee7d7b01febadf68ddd1ff5fa4e351c39
lnh <lnhdyx@outlook.com>
2026-05-20 00:51:28 +0800

    refactor cache
    --ci

--- .hugo/static/asset/tickflow.js
+++ .hugo/static/asset/tickflow.js
@@ -47,39 +47,23 @@
         return query.join("&");
     }
 
-    function getCache(key) {
-        if (!window.localStorage) {
-            return
-        }
-        return localStorage.getItem(key);
-    }
-
-    function setCache(key, value) {
-        if (!window.localStorage) {
-            return
-        }
-        try {
-            localStorage.setItem(key, value);
-        } catch (e) {
-            console.error("setCache error", e);
-        }
-    }
-
     function v1InstrumentsAsync(param, callback) {
         var headers = buildHeaders(param);
         var url = apiHost + "/v1/instruments?symbols=" + param.symbol;
-        httpGetJsonAsync(url, headers, callback);
+        var cacheKey="v1_instruments_" + param.symbol;
+        httpGetJsonAsync(cacheKey,url, headers, callback);
     }
 
     function v1KlinesAsync(param, callback) {
         var headers = buildHeaders(param);
         var queryString = buildQueryString(param, ["symbol", "period", "count", "start_time", "end_time", "adjust"]);
         var url = apiHost + "/v1/klines?" + queryString;
-        httpGetJsonAsync(url, headers, callback);
+        var cacheKey="v1_klines_" + param.symbol + "_" + param.period;
+        httpGetJsonAsync(cacheKey,url, headers, callback);
     }
 
-    function httpGetJsonAsync(url, headers, callback) {
-        var cache = getCache(url);
+    function httpGetJsonAsync(cacheKey, url, headers, callback) {
+        var cache = blogCache.getCache(cacheKey);
         if (cache) {
             callback(JSON.parse(cache));
             return;
@@ -88,7 +72,7 @@
         xhr.onreadystatechange = function (e) {
             if (xhr.readyState === XMLHttpRequest.DONE) {
                 if (xhr.responseText) {
-                    setCache(url, xhr.responseText);
+                    blogCache.setCache(cacheKey, xhr.responseText);
                 }
                 callback(JSON.parse(xhr.responseText));
             }
--- src/tool/stock-kline/app.js
+++ src/tool/stock-kline/app.js
@@ -1,3 +1,4 @@
+var TICK_FLOW_API_KEY_CACHE_KEY = "stockKLineTickFlowApiKey";
 var klineChart = echarts.init(this.klineChartDiv);
 var klineMacdChart = echarts.init(this.klineMacdChartDiv);
 var klineCountChart = echarts.init(this.klineCountChartDiv);
@@ -7,25 +8,30 @@ var charts = [klineChart, klineMacdChart, klineCountChart, klineOvbChart];
 onChartDispatchDataZoom(charts);
 onChartDispatchToolTip(charts);
 
-blog.tryLoadInputValueFromLocalStorage("stockKLineTickFlowApiKey");
-blog.tryLoadInputValueFromLocalStorage("stockKLineSymbol");
+blog.tryLoadInputValueFromLocalStorage(TICK_FLOW_API_KEY_CACHE_KEY);
 
-function removeCache() {
-    if (!window.localStorage) {
-        return;
+function loadStockKLineSymbols() {
+    var symbolList = blogCache.getCacheObject("stockKLineSymbol");
+    if (symbolList) {
+        var datalist = document.getElementById("stockKLineSymbolList");
+        datalist.innerHTML = "";
+        symbolList.forEach(function (symbol) {
+            var option = document.createElement("option");
+            option.value = symbol;
+            option.innerHTML = symbol;
+            datalist.appendChild(option);
+        });
     }
-    var stockKLineTickFlowApiKeyValue = localStorage.getItem("stockKLineTickFlowApiKey");
-    localStorage.clear();
-    localStorage.setItem("stockKLineTickFlowApiKey", stockKLineTickFlowApiKeyValue);
 }
 
 function renderKLine() {
     var param = {
-        key: document.getElementById("stockKLineTickFlowApiKey").value,
+        key: document.getElementById(TICK_FLOW_API_KEY_CACHE_KEY).value,
         symbol: document.getElementById("stockKLineSymbol").value
     };
-    blog.trySaveInputValueToLocalStorage("stockKLineTickFlowApiKey");
-    blog.trySaveInputValueToLocalStorage("stockKLineSymbol");
+    blog.trySaveInputValueToLocalStorage(TICK_FLOW_API_KEY_CACHE_KEY);
+    blogCache.addSet("stockKLineSymbol", param.symbol);
+    loadStockKLineSymbols();
     document.getElementById("renderButton").disabled = "disabled";
     getKlineData(param, function (data) {
         console.log("getKlineData", data);
